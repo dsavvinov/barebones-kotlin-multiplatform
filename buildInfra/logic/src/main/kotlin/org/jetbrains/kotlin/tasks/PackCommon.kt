4555ddc77513131d02bc9dbd88898cdee17cb615
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2022-10-24 17:01:45 +0200

    Extract JAR-packing to utility function

--- buildInfra/logic/src/main/kotlin/org/jetbrains/kotlin/tasks/PackCommon.kt
+++ buildInfra/logic/src/main/kotlin/org/jetbrains/kotlin/tasks/PackCommon.kt
@@ -1,24 +1,13 @@
 package org.jetbrains.kotlin.tasks
 
 import org.jetbrains.kotlin.hardcode.HardcodedPaths
-import org.jetbrains.kotlin.jarOutputStream
-import java.util.jar.JarEntry
-import kotlin.io.path.relativeTo
+import org.jetbrains.kotlin.packJar
 
 object PackCommon : KmpBuildTask("PACK_COMMON") {
     override fun execute(paths: HardcodedPaths) {
-        val jarDestination = paths.Outputs().Packed().commonJar
-        jarDestination.delete()
-        jarDestination.parentFile.mkdirs()
-
-        jarDestination.jarOutputStream().use { os ->
-            val root = paths.Outputs().AssembledBinaries().commonMain
-            val rootPath = root.toPath()
-            root.walk().filter { it.isFile }.forEach { file ->
-                os.putNextEntry(JarEntry(file.toPath().relativeTo(rootPath).toString()))
-                os.write(file.readBytes())
-            }
-            os.finish()
-        }
+        packJar(
+            destination = paths.Outputs().Packed().commonJar,
+            sources = listOf(paths.Outputs().AssembledBinaries().commonMain)
+        )
     }
 }
--- buildInfra/logic/src/main/kotlin/org/jetbrains/kotlin/utils.kt
+++ buildInfra/logic/src/main/kotlin/org/jetbrains/kotlin/utils.kt
@@ -2,7 +2,9 @@ package org.jetbrains.kotlin
 
 import java.io.File
 import java.io.FileOutputStream
+import java.util.jar.JarEntry
 import java.util.jar.JarOutputStream
+import kotlin.io.path.relativeTo
 
 fun File.assertExists(): File {
     require(exists()) { "File $absolutePath not found" }
@@ -14,3 +16,24 @@ fun <K, V> Iterable<Pair<K, V>>.toMutlivalueMap(): Map<K, List<V>> {
 }
 
 fun File.jarOutputStream(): JarOutputStream = JarOutputStream(outputStream())
+
+/**
+ * Creates a *new* JAR-file at [destination] with [sources]
+ * being places at top-level. If [sources] is a directory,
+ * all it's content packed as well.
+ */
+fun packJar(destination: File, sources: List<File>) {
+    destination.delete()
+    destination.parentFile.mkdirs()
+
+    destination.jarOutputStream().use { os ->
+        for (root in sources) {
+            val rootPath = root.toPath()
+            root.walk().filter { it.isFile }.forEach { file ->
+                os.putNextEntry(JarEntry(file.toPath().relativeTo(rootPath).toString()))
+                os.write(file.readBytes())
+            }
+            os.finish()
+        }
+    }
+}
